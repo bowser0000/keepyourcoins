c3db46e4189eed55e4820b026d45840f2334b996
Brahmjot Grewal <brahmjotgrewal@hotmail.com>
2016-04-11 15:03:04 -0400

    Added Bath, XBOX Gold

--- game.js
+++ game.js
@@ -10,6 +10,15 @@ function Game() {
   alert('You wake up, and find ' + var1 + ' coin(s) on your bed!');
   var coins = 0 + var1;
   alert('You now have ' + coins + ' coin(s)!');
+  alert('You get out of bed and take a bath.');
+  var var2 = RandomDamage(1, 3);
+  alert('While taking off your pants, you find ' + var2 + ' coin(s) in pocket!');
+  coins = coins + var2;
+  alert('You now have ' + coins + ' coins!');
+  alert('You go downstairs and find your XBOX gold ran out.');
+  alert('You pay 6 coins and play some Titanfall.');
+  coins = coins - 6;
+  alert('You now have ' + coins + ' coin(s)!');
 }
 
 Game();
