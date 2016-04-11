6becddb2f881841a2485675fdf510efc0457f6d1
Brahmjot Grewal <brahmjotgrewal@hotmail.com>
2016-04-11 14:03:49 -0400

    Coins on counter and random saving

--- game.js
+++ game.js
@@ -4,8 +4,12 @@ function RandomDamage(min, max) {
 return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+var var1 = RandomDamage(1, 7);
+var coins = 0;
 function Game() {
-  
+  alert('You wake up, and find ' + var1 + ' coin(s) on your bed!');
+  var coins = 0 + var1;
+  alert('You now have ' + coins + ' coin(s)!');
 }
 
 Game();
