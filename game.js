2138f731db0a814cd92acb51cb233d4dcf6ca6cb
Brahmjot Grewal <brahmjotgrewal@hotmail.com>
2016-04-11 23:02:00 -0400

    Left path, right path

--- game.js
+++ game.js
@@ -5,6 +5,8 @@ return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 var var1 = RandomDamage(1, 7);
+var path1a = Math.random();
+
 var coins = 0;
 function Game() {
   alert('You wake up, and find ' + var1 + ' coin(s) on your bed!');
@@ -19,6 +21,39 @@ function Game() {
   alert('You pay 6 coins and play some Titanfall.');
   coins = coins - 6;
   alert('You now have ' + coins + ' coin(s)!');
+  alert('You walk outside. There is a path to the left and one to the right.');
+  var path1 = prompt('Do you take the [left] path or the [right] path?').lower();
+  if (path1 == 'left') {
+  	if(path1a => 0.25) {
+  		alert('While walking, you find a monster.');
+  		var path1m = prompt('Do you [run] or [attack]?').lower();
+  		if (path1m == 'run') {
+  			
+  		} else if (path1m == 'attack') {
+  			
+  		} else {
+  			throw new Error('Use [run] or [attack]!');
+  		}
+  	} else {
+  		
+  	}
+  } else if (path1 == 'right') {
+  	if(path1a => 0.25) {
+  		alert('While walking, you find a monster.');
+  		var path1ma = prompt('Do you [run] or [attack]?').lower();
+  		if (path1ma == 'run') {
+  			alert('You run away, but you drop ' + RandomDamage(3, 7) + ' coins on the way.');
+  		} else if (path1ma == 'attack') {
+  			
+  		} else {
+  			throw new Error('Use [run] or [attack]!');
+  		}
+  	} else {
+  		
+  	}
+  } else {
+  	throw new Error('Please use [left] or [right]!');
+  }
   throw new Error('Work In Progress!');
 }
 
