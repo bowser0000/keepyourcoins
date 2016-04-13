b01715dd77ee169481d57e64b2e17bba7167a35e
Brahmjot Grewal <brahmjotgrewal@hotmail.com>
2016-04-12 21:01:02 -0400

    Added monster fight, random vars for it, and Game2()

--- game.js
+++ game.js
@@ -6,11 +6,17 @@ return Math.floor(Math.random() * (max - min + 1)) + min;
 
 var var1 = RandomDamage(1, 7);
 var path1a = Math.random();
+var path1lose = Math.random();
+var xd123 = RandomDamage(3, 7);
+var xd12344 = RandomDamage(1, 5);
+var coins = 0 + var1;
 
-var coins = 0;
+function Game2() {
+	throw new Error('Work In Progress!');
+}
+
 function Game() {
   alert('You wake up, and find ' + var1 + ' coin(s) on your bed!');
-  var coins = 0 + var1;
   alert('You now have ' + coins + ' coin(s)!');
   alert('You get out of bed and take a bath.');
   var var2 = RandomDamage(1, 3);
@@ -22,38 +28,57 @@ function Game() {
   coins = coins - 6;
   alert('You now have ' + coins + ' coin(s)!');
   alert('You walk outside. There is a path to the left and one to the right.');
-  var path1 = prompt('Do you take the [left] path or the [right] path?').lower();
+  var path1 = prompt('Do you take the [left] path or the [right] path?').toLowerCase();
   if (path1 == 'left') {
-  	if(path1a => 0.25) {
+		if(path1a >= 0.25) {
   		alert('While walking, you find a monster.');
-  		var path1m = prompt('Do you [run] or [attack]?').lower();
-  		if (path1m == 'run') {
-  			
-  		} else if (path1m == 'attack') {
-  			
-  		} else {
-  			throw new Error('Use [run] or [attack]!');
+  		var path1ma = prompt('Do you [run] or [attack]?').toLowerCase();
+  		if (path1ma == 'run') {
+  			alert('You run away, but you drop ' + xd123 + ' coins on the way.');
+  			Game2();
+  		} else if (path1ma == 'attack') {
+  			alert('You managed to fight off the monster.');
+  			if (path1lose <= 0.25) {
+  				alert('While fighting, you see that you lost 2 coins.');
+  				coins = coins - 2;
+  			} else {
+  				alert('You find ' + xd12344 + ' coins!');
+  				coins = coins + xd12344;
+  				Game2();
+  			}
   		}
+
   	} else {
-  		
+  		throw new Error('Use [run] or [attack]!');
+  	}
+  	} else {
+
   	}
   } else if (path1 == 'right') {
-  	if(path1a => 0.25) {
+  	if(path1a >= 0.25) {
   		alert('While walking, you find a monster.');
-  		var path1ma = prompt('Do you [run] or [attack]?').lower();
+  		var path1ma = prompt('Do you [run] or [attack]?').toLowerCase();
   		if (path1ma == 'run') {
-  			alert('You run away, but you drop ' + RandomDamage(3, 7) + ' coins on the way.');
+  			alert('You run away, but you drop ' + xd123 + ' coins on the way.');
+  			Game2();
   		} else if (path1ma == 'attack') {
-  			
-  		} else {
-  			throw new Error('Use [run] or [attack]!');
+  			alert('You managed to fight off the monster.');
+  			if (path1lose <= 0.25) {
+  				alert('While fighting, you see that you lost 2 coins.');
+  				coins = coins - 2;
+  			} else {
+  				alert('You find ' + xd12344 + ' coins!');
+  				coins = coins + xd12344;
+  				Game2();
+  			}
   		}
+
   	} else {
-  		
+  		throw new Error('Use [run] or [attack]!');
   	}
-  } else {
-  	throw new Error('Please use [left] or [right]!');
-  }
+  	} else {
+
+  	}
   throw new Error('Work In Progress!');
 }
 
