f4d7b5c50898f97ea8bb014c3b08144ee62ebb7f
bowser0000 <brahmjotgrewal@hotmail.com>
2016-04-17 18:35:32 -0400

    Bunch of changes

--- game.js
+++ game.js
@@ -65,14 +65,54 @@ var path1a = Math.random();
 var path1lose = Math.random();
 var xd123 = RandomDamage(3, 7);
 var xd12344 = RandomDamage(1, 5);
-var weapon = 'Wooden_Sword';
+var kill2 = Math.random();
+var killwin = RandomDamage(10, 15);
+var weapon = 'Wooden Sword';
 var coins = var1;
+var battleswoodensword = 0;
+var battlesironsword = 0;
+var battlesdiamondsword = 0;
+var battlesemeraldsword = 0;
+var battlesharshansword = 0;
+var battlesmrmaddonsword = 0;
+var woodenswordfire = 0;
+var woodenswordfreeze = 0;
+var ironswordfire = 0;
+var ironswordfreeze = 0;
+var diamondswordfire = 0;
+var diamondswordfreeze = 0;
+var emeraldswordfire = 0;
+var emeraldswordfreeze = 0;
+var totalbattles = 0;
+
+function Game3() {
+	alert('You have ' + coins + ' coin(s!)');
+	throw new Error('Work In Progress!');
+}
 
 function Game2() {
 	alert('You continue on the road.');
 	alert('You find a little shop on the side');
-	alert('He says \'It\'s Christmas!\' He gives you a wooden sword for free!');
-	throw new Error('Work In Progress!');
+	alert('He says \"It\'s Christmas!\" He gives you a wooden sword for free!');
+	alert('You inspect your ' + weapon);
+  alert('Weapon: ' + weapon + '\nDamage: 1\nBattles Fought With: ' + battleswoodensword + '\nBattles Fought: ' + totalbattles);
+	alert('The shop keeper tells you \'Hey, go kill that guys over there with that sword, and I\'ll give you a few coins ;)\'');
+	var kill = prompt('Do you [kill him] or [do not]?').toLowerCase();
+	if (kill == 'kill him') {
+		if (kill2 >= 0.25) {
+			alert('The guy manages to fight back, and kills you.\nGame Over.');
+			throw new Error ('You died!');
+		} else {
+			alert('You kill kill him, and the shop keeper gives you ' + killwin + ' coins!');
+			coins = coins + killwin;
+			Game3();
+		}
+	} else if (kill == 'do not') {
+		alert('You don\' kill him, and the shopkeeper gives you no coins.');
+		Game3();
+	} else {
+		throw new Error('Use [kill him] or [do not]!');
+	}
 }
 
 function Game() {
@@ -90,6 +130,7 @@ function Game() {
   var path1 = prompt('Do you take the [left] path or the [right] path?').toLowerCase();
   if (path1 == 'left') {
 		if(path1a >= 0.25) {
+			totalbattles = 1;
   		alert('While walking, you find a monster.');
   		var path1ma = prompt('Do you [run] or [attack]?').toLowerCase();
   		if (path1ma == 'run') {
@@ -112,6 +153,7 @@ function Game() {
   	}
   } else if (path1 == 'right') {
   	if(path1a >= 0.25) {
+			totalbattles = 1;
   		alert('While walking, you find a monster.');
   		var path1ma1 = prompt('Do you [run] or [attack]?').toLowerCase();
   		if (path1ma1 == 'run') {
